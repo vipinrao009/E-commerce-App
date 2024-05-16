43b669c1f6667283285125f9cd644d9b2755958b
vipinrao009 <vipinrao2018@gmail.com>
2024-05-16 15:45:14 +0530

    Added hashed password & compared password

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+
+
+const registerUser = async(req,res)=>{
+
+}
+
+export {
+    registerUser
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.connection.js";
 import morgan from "morgan";
+import user from "./routes/userRoutes.js"
 
 //configure env
 dotenv.config();
@@ -16,6 +17,9 @@ const app = express();
 //middleware
 app.use(express.json())
 app.use(morgan('dev'))
+
+//routes
+app.use("/api/v1/user",user)
 
 app.use("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce website</h1>");

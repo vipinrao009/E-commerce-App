151a3a335616c17f1075d18500d1d39559b1e76b
vipinrao009 <vipinrao2018@gmail.com>
2024-09-25 13:23:03 +0530

    Added fuctionality to auto switch accourding to the Local or Production

--- client/src/Layout/BaseUrl.js
+++ client/src/Layout/BaseUrl.js
@@ -1,1 +1,6 @@
-export const baseUrl = "https://e-commerce-app-1-zuls.onrender.com"
+// config.js
+const environment = process.env.NODE_ENV;  // Get current environment ('development' or 'production')
+
+export const baseUrl = environment === 'development'
+  ? "http://localhost:8080"  // Local development URL
+  : "https://e-commerce-app-1-zuls.onrender.com";  // Production URL
